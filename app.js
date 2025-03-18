d3d809c70baa7a232ab223cbed25248fd644a9d0
Flemecs <164572590+Flemacs@users.noreply.github.com>
2025-03-18 00:53:16 -0500

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre=document.getElementById("nombreAmigo").value.trim();
+
+    if (nombre === "") {
+        alert("ingrese los Nombre.");
+        
+    } else {
+        amigos.push(nombre);
+        document.getElementById("nombreAmigo").value= ""; 
+        mostrarListaAmigos();
+    }
+}
+
+function mostrarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    amigos.forEach(function(amigo){
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Debe de ingresar mas de 2 amigos.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = "El amigo o La amiga secreto(a) es: "+ amigoSecreto;
+}
